9cef8849e3ee5ba572e0335ddf8555ad361e1088
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2024-06-03 15:50:35 +0300

    add styles for info panel

--- src/components/InfoPanel/InfoPanel.jsx
+++ src/components/InfoPanel/InfoPanel.jsx
@@ -5,19 +5,19 @@ const InfoPanel = () => {
     <ul className={css.infoPanelList}>
       <li className={css.listItem}>
         <h2>IP ADDRESS</h2>
-        <p>–</p>
+        <p>192.212.174.101</p>
       </li>
       <li className={css.listItem}>
         <h2>LOCATION</h2>
-        <p>–</p>
+        <p>Brooklyn, NY 10001</p>
       </li>
       <li className={css.listItem}>
         <h2>TIMEZONE</h2>
-        <p>–</p>
+        <p>UTC -05:00</p>
       </li>
       <li className={css.listItem}>
         <h2>ISP</h2>
-        <p>–</p>
+        <p>SpaceX Starlink</p>
       </li>
     </ul>
   )
