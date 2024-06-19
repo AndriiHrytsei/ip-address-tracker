f5942f0762198c86afc19afe0fcd871849046dcb
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2024-06-19 21:00:45 +0300

    implement typescript

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,12 @@
+import React from "react"
 import css from "./Layout.module.css"
-import PropTypes from 'prop-types';
 
-const Layout = ({ children }) => {
+const Layout = ({ children }: {children: React.ReactNode}) => {
   return (
     <section className={css.layout}>
       {children}
     </section>
   )
-}
-
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired
 }
 
 export default Layout
