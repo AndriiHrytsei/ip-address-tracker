e142e78c0be1892d6d1330d551eefb843aa89f41
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2024-06-20 16:43:15 +0300

    add types/ directory for custom types and interfaces; type some variables that i missed last time

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,15 +3,14 @@ import Form from "../Form/Form";
 import Layout from "../Layout/Layout";
 import { useState } from "react";
 import MapMarker from "../MapMarker/MapMarker";
-
-type LatLng = [number, number]
+import LatLng from "../../types/latLng";
 
 const App = () => {
   const [latLng, setLatLng] = useState<LatLng>([0, 0]);
 
   return (
     <Layout>
-      <Form getLatLng={(data: [number, number]): void => setLatLng(data)} />
+      <Form getLatLng={(data: LatLng): void => setLatLng(data)} />
       <Map>
         <MapMarker latLng={latLng} />
       </Map>
---
+++ src/types/geolocation.ts
@@ -0,0 +1,10 @@
+interface GeoLocationInfoInterface {
+  ip: string;
+  city: string;
+  province: string;
+  zipcode: string;
+  isp: string;
+  timezoneOffset: number;
+}
+
+export default GeoLocationInfoInterface
